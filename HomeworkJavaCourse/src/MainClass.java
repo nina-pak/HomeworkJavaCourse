6209876f90552fd84384d7ed628d327c140c920e
nina_pak <nina.p_a@mail.ru>
2024-10-22 01:19:32 +0400

    Homework Test 1.2

--- HomeworkJavaCourse/src/MainClass.java
+++ HomeworkJavaCourse/src/MainClass.java
@@ -1,8 +1,17 @@
 public class MainClass
 {
-    public int getLocalNumber()
+    /*
+    public int getLocalNumber()     //Homework Test 1.1
     {
         int number = 14;
         return number;
     }
+    */
+
+    private int class_number = 20;          //Homework Test 1.2
+    public int getClassNumber(int class_number)
+    {
+        return class_number;
+    }
+    int number = getClassNumber(class_number);
 }
--- HomeworkJavaCourse/src/MainClassTest.java
+++ HomeworkJavaCourse/src/MainClassTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 public class MainClassTest extends MainClass
 {
 
-    @Test
+    /*
+    @Test           //Homework Test 1.1
     public void testGetLocalNumber()
     {
         int expected = 14;
         int actual = this.getLocalNumber();
         Assert.assertTrue("The number is not equal to 14", expected==actual);
     }
+    */
 
+    @Test           //Homework Test 1.2
+    public void testGetClassNumber()
+    {
+        int expected = 45;
+        int actual = this.number;
+        Assert.assertTrue("The number is less than 45", actual > expected);
+    }
 }
