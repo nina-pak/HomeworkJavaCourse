cf142b5e265879b2b9db5d09f954ff576dd54060
nina_pak <nina.p_a@mail.ru>
2024-12-10 03:07:50 +0400

    Homework Test 1.3

--- HomeworkJavaCourse/src/MainClass.java
+++ HomeworkJavaCourse/src/MainClass.java
@@ -8,10 +8,18 @@ public class MainClass
     }
     */
 
-    private int class_number = 20;          //Homework Test 1.2
+    /*private int class_number = 20;          //Homework Test 1.2
     public int getClassNumber(int class_number)
     {
         return class_number;
     }
-    int number = getClassNumber(class_number);
+    int number = getClassNumber(class_number); */
+
+    private String class_string = "Hello, world";       //Homework Test 1.3
+    public String getClassString(String class_string)
+    {
+        return class_string;
+    }
+    String test_string = getClassString(class_string);
+
 }
--- HomeworkJavaCourse/src/MainClassTest.java
+++ HomeworkJavaCourse/src/MainClassTest.java
@@ -1,8 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass
-{
+public class MainClassTest extends MainClass {
 
     /*
     @Test           //Homework Test 1.1
@@ -14,11 +13,23 @@ public class MainClassTest extends MainClass
     }
     */
 
+    /*
     @Test           //Homework Test 1.2
     public void testGetClassNumber()
     {
         int expected = 45;
         int actual = this.number;
         Assert.assertTrue("The number is less than 45", actual > expected);
+    }
+    */
+
+    @Test           //Homework Test 1.3
+    public void testGetClassString() {
+
+        Assert.assertTrue("There is no word \"Hello\" in this line", contains());
+    }
+
+    private boolean contains() {
+        return test_string.toLowerCase().contains("Hello".toLowerCase());
     }
 }
